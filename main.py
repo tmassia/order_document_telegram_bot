83d567a51bc61a4bf1088527c21e779ca19c0dec
Tatiana <taniamassya@gmail.com>
2023-10-25 14:24:07 +0300

    1-var.sender moved from func.file to .env 2-timeout added to handler to resolve timeout error

--- main.py
+++ main.py
@@ -4,57 +4,68 @@ from dotenv import load_dotenv
 import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
+from send_email import send_email_with_attachment
 
 load_dotenv()
+
+email_to = os.getenv('SMTP_reciever')
+subject = "Here is your document"
+body = "Please find the attached document."
+app_password = os.getenv('SMTP_APP_PASSWORD')
+sender_email = os.getenv('SMTP_sender')
 
 # DATABASE_URL = os.getenv("DATABASE_URL")
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-downloads_directory = '/mnt/e/Tatiana/PROJs/TELEGRAMM/document_send_bot/downloads'
+downloads_directory = os.getenv('directory')
 
 
 # Основная команда /start
 @dp.message(F.text == '/start')
 async def cmd_start(message: Message):
-    await message.answer('ברוכים הבאים לאייקון מעבדות נא לעלות מסמך!')
+    await message.answer('Hello in Doc_send_bot!You can send me your document and I will send it to you')
 
 
 # Получение id/имени с помощью message.from_user.id
 @dp.message(F.text == '/my_id')
 async def cmd_my_id(message: Message):
-    await message.answer(f'Ваш ID: {message.from_user.id}')
-    await message.reply(f'Ваше имя: {message.from_user.first_name}')
+    await message.answer(f'Your ID: {message.from_user.id}')
+    await message.reply(f'Your name: {message.from_user.first_name}')
 
 
 @dp.message(F.document)
 async def get_document(message: Message):
-    # Получите информацию о файле
-    file_info = await bot.get_file(message.document.file_id)
+    # Get info about file
+    file_info = await bot.get_file(message.document.file_id, request_timeout=60)
     file_path_on_telegram = file_info.file_path
 
-    # Определите имя файла
+    # name of file
     file_name = os.path.basename(file_path_on_telegram)
 
-    # Полный путь для сохранения файла на вашем сервере
+    # full path to file and name
     destination_path = os.path.join(downloads_directory, file_name)
 
-    # Скачайте и сохраните файл
+    # Download file and save it
     await bot.download_file(file_path_on_telegram, destination_path)
+    try:
+        await send_email_with_attachment(email_to, subject, body, destination_path, app_password, sender_email)
+        # send message answer with file id
+        await message.answer(
+            f"File with id {message.document.file_id} successful downloaded and saved as  {destination_path}")
+    except Exception as e:
+        logger.error(f"An error occurred while processing the document: {str(e)}")
+        await message.answer(f"An error occurred while sending the document: {str(e)}")
 
-    # Отправьте ответное сообщение с идентификатором файла
-    await message.answer(
-        f"Файл с идентификатором {message.document.file_id} успешно скачан и сохранен как {destination_path}")
 
-
-# Хэндлер без фильтра, сработает, если ни один выше не сработает.
+# handler for any other message
 @dp.message()
 async def echo(message: Message):
-    await message.answer('Я тебя не понимаю...')
+    await message.answer('I dont understand you..')
 
 
-# Polling, т.е бесконечный цикл проверки апдейтов
+# Polling, updates cycle
 async def main():
     await dp.start_polling(bot)
 
@@ -62,6 +73,7 @@ async def main():
 if __name__ == '__main__':
     try:
         logging.basicConfig(level=logging.INFO)
+        logger = logging.getLogger(__name__)
         asyncio.run(main())
     except KeyboardInterrupt:
         print('Exit')
